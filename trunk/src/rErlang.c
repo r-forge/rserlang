82b1f7ce2530a647fb2d260e9941b0e65392b371
keisuke (Keisuke Honda) <keisukeh@gmail.com>
2009-09-20 06:24:05 +0000

    modify
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rserlang@63 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/src/rErlang.c
+++ trunk/src/rErlang.c
@@ -2,21 +2,24 @@
 #include "ei.h"
 #include "rErlang.h"
 
-//internal func.
-int rE_init(){
-  printf("erl_init\n");
-  
-  erl_init(NULL,0);
-
+int rE_init(){  
+  erl_init(NULL,0);  
   return 1;
 }
 
-//
 int rE_connect(){  
   rE_init();
-  
+
+  int identification_number = 99;
+  int creation=1;
+  char *cookie="rerlang"; 
+  erl_connect_init(identification_number, cookie, creation);
+
+  int sockfd;
+  char *nodename="node01@localhost"; 
+  if ((sockfd = erl_connect(nodename)) < 0){
+    erl_err_quit("ERROR: erl_connect failed");   
+    return 0;
+  }
   return 1;
 }
-
-
-
