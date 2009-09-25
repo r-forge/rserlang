83cca880599266eb7ae07cd862de00d3cdf15c0f
keisuke (Keisuke Honda) <keisukeh@gmail.com>
2009-09-25 06:48:49 +0000

    modify
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rserlang@81 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -59,6 +59,34 @@ And also we want to ensure that rErlang is robust in the event that the external
 <p>Coming Soon !</p>
 
 <h2>Tutorial</h2>
+<h3>Erlang -> R</h3>
+</td></tr></table>
+<table border=0 width=100%><tr><td>
+<table bgcolor=black width=100% border=1 cellspacing=0><tr><td><font color=white><pre>
+usr@home: erl
+Erlang (BEAM) emulator version 5.6.3 [source] [smp:2] [async-threads:0] [kernel-poll:false]
+
+Eshell V5.6.3  (abort with ^G)
+1> eri:start().
+<0.33.0>
+2> eri:connect().
+{ok,0}
+3> eri:eval("1:9").
+{ok,'INTSXP',[1,2,3,4,5,6,7,8,9]}
+4> {ok, Type, Result} = eri:eval("rnorm(10)").
+5> Type.  
+'REALSXP'
+6> Result.
+[-0.1917861903879111,1.0077673823951807,0.05707066688405165,
+ -1.7174303197492615,0.6188926676115155,0.25904560550869155,
+ 0.5981845165498236,-0.4449771878757933,1.0737026013065656,
+ 0.22614169517088667]
+</pre></font></td></tr></table>
+</td></tr></table>
 <h3>R -> Erlang</h3>
+<table border=0 width=100%><tr><td>
+<table bgcolor=black width=100% border=1 cellspacing=0><tr><td><font color=white><pre>
+</pre></font></td></tr></table>
+
 </body>
 </html>
